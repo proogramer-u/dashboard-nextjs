2f826d9e721cb4ac8df9ca9f43db392f10e2b2b8
proogramer-u <cok.iswara2007@gmail.com>
2026-09-10 10:15:45 +0700

    fixed bug

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,19 +1,36 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+import { useSearchParams,usePathname,useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
 
-  function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
+    params.set('page','1');
     if (term) {
-      params.set('query',term);
+      params.set('query',term)
     } else {
-      params.delete('query')
+      params.delete('query');
     }
-  }
+    replace(`${pathname}?${params.toString()}`);
+  },300);
+
+  // function handleSearch(term: string) {
+  //   console.log(`searching... ${term}`);
+  //   const params = new URLSearchParams(searchParams);
+  //   if (term) {
+  //     params.set('query',term);
+  //   } else {
+  //     params.delete('query')
+  //   }
+  //   replace(`${pathname}?${params.toString()}`);
+  // }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -25,6 +42,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e)=>{
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
